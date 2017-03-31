608e2fe5d35a5ecacf4128849196911e8a7c5c59
MikeStegall <mestegall5@gmail.com>
2017-03-30 20:23:54 -0500

    finished exercise3 and exercise4

--- excercise3/js/app.js
+++ excercise3/js/app.js
@@ -1,9 +1,13 @@
 var checkboxes = document.querySelectorAll('input')
 
-var customAlert = function (event) {
+// var customAlert = function (event) {
+//   alert(event.target.value)
+// }
+
+function customAlert (event) {
   alert(event.target.value)
 }
 
 checkboxes.forEach(function (checkbox) {
-  checkbox // add event listener
+  checkbox.addEventListener('click', customAlert) // add event listener
 })
